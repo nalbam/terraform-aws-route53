075cd9ea03bffbf305f564f49a1ad03f6682e3d7
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2018-03-07 09:54:45 +0900

    acm_certificate_id

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "route53_zone_id" {
   value = "${data.aws_route53_zone.default.id}"
 }
 
+output "acm_certificate_id" {
+  value = "${data.aws_acm_certificate.default.id}"
+}
+
 output "acm_certificate_arn" {
   value = "${data.aws_acm_certificate.default.arn}"
 }
