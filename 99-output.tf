7e8dbc17a958826eb180e22f950123edbb8f992f
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-09-26 12:07:24 +0900

    cert

--- 99-output.tf
+++ 99-output.tf
@@ -9,9 +9,9 @@ output "name" {
 }
 
 output "certificate_id" {
-  value = var.acm_certificate ? aws_acm_certificate.this[0].id : data.aws_acm_certificate.this[0].id
+  value = var.acm_certificate ? aws_acm_certificate.cert[0].id : data.aws_acm_certificate.this[0].id
 }
 
 output "certificate_arn" {
-  value = var.acm_certificate ? data.aws_acm_certificate.this[0].arn : data.aws_acm_certificate.this[0].arn
+  value = var.acm_certificate ? data.aws_acm_certificate.cert[0].arn : data.aws_acm_certificate.this[0].arn
 }
