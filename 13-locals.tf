08207602f025677ab0f99de46773a245b409a737
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-09-26 15:22:05 +0900

    local

---
+++ 13-locals.tf
@@ -0,0 +1,6 @@
+# locals
+
+locals {
+  certificate_id  = var.acm_certificate ? element(concat(aws_acm_certificate.cert.*.id, [""]), 0) : element(concat(data.aws_acm_certificate.this.*.id, [""]), 0)
+  certificate_arn = var.acm_certificate ? element(concat(aws_acm_certificate.cert.*.arn, [""]), 0) : element(concat(data.aws_acm_certificate.this.*.arn, [""]), 0)
+}
