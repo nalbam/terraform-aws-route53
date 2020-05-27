74fd455a0673dea637b61426467a9be62c25a9dc
nalbam <me@nalbam.com>
2020-05-27 17:08:50 +0900

    depends_on

--- 52-acm.tf
+++ 52-acm.tf
@@ -24,6 +24,10 @@ resource "aws_route53_record" "cert" {
   records = [
     aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_value,
   ]
+
+  depends_on = [
+    aws_acm_certificate.cert,
+  ]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
@@ -36,6 +40,7 @@ resource "aws_acm_certificate_validation" "cert" {
   ]
 
   depends_on = [
+    aws_acm_certificate.cert,
     aws_route53_record.cert,
   ]
 }
