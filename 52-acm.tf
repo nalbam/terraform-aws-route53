9b35275f481c1e8fbebaf6ac194f0a306deb7d85
nalbam <me@nalbam.com>
2020-05-27 16:52:34 +0900

    terraform array

--- 52-acm.tf
+++ 52-acm.tf
@@ -17,21 +17,21 @@ resource "aws_route53_record" "cert" {
 
   zone_id = data.aws_route53_zone.this.id
 
-  name = aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_name
-  type = aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_type
+  name = aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_name
+  type = aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_type
   ttl  = 60
 
   records = [
-    aws_acm_certificate.cert[0].domain_validation_options[0].resource_record_value,
+    aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_value,
   ]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   count = var.acm_certificate ? 1 : 0
 
-  certificate_arn = aws_acm_certificate.cert[0].arn
+  certificate_arn = aws_acm_certificate.cert.0.arn
 
   validation_record_fqdns = [
-    aws_route53_record.cert[0].fqdn,
+    aws_route53_record.cert.0.fqdn,
   ]
 }
