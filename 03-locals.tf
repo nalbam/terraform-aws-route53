ceee3db943ef2c07a0e07911cc843b036411b0c3
nalbam <me@nalbam.com>
2020-05-28 15:41:11 +0900

    local.certificate_arn

--- 03-locals.tf
+++ 03-locals.tf
@@ -4,6 +4,6 @@ locals {
   domain_name = var.domain_name == "" ? var.root_domain : var.domain_name
 }
 
-# locals {
-#   certificate_arn = var.acm_certificate ? aws_acm_certificate_validation.cert.0.certificate_arn : data.aws_acm_certificate.this.0.arn
-# }
+locals {
+  certificate_arn = var.acm_certificate ? join("", aws_acm_certificate_validation.cert.*.certificate_arn) : join("", data.aws_acm_certificate.this.*.arn)
+}
--- 99-output.tf
+++ 99-output.tf
@@ -9,7 +9,7 @@ output "name" {
 }
 
 output "certificate_arn" {
-  # value = local.certificate_arn
+  value = local.certificate_arn
   # value = aws_acm_certificate_validation.cert.0.certificate_arn
-  value = join("", aws_acm_certificate_validation.cert.*.certificate_arn)
+  # value = join("", aws_acm_certificate_validation.cert.*.certificate_arn)
 }
