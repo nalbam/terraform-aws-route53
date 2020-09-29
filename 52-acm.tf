ba1f07ea0d93fb375f21203ee7f1ebeb5247b6ea
nalbam <me@nalbam.com>
2020-09-29 13:50:01 +0900

    element concat

--- 52-acm.tf
+++ 52-acm.tf
@@ -20,13 +20,12 @@ resource "aws_route53_record" "cert" {
   count = var.acm_certificate ? 1 : 0
 
   zone_id = data.aws_route53_zone.this.id
-
-  name = aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_name
-  type = aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_type
-  ttl  = 60
+  name    = element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_name, [""]), 0)
+  type    = element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_type, [""]), 0)
+  ttl     = 300
 
   records = [
-    aws_acm_certificate.cert.0.domain_validation_options.0.resource_record_value,
+    element(concat(aws_acm_certificate.cert.0.domain_validation_options.*.resource_record_value, [""]), 0),
   ]
 }
 
